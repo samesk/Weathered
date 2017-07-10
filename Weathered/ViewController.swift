321b28117a11d4f40f5491b7ecd9df5d17fada56
Samuel Eskenasy <samueleskenasy@samuels-macbook-air.local>
2017-07-10 14:49:11 +0300

    Initial Commit

---
+++ Weathered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weathered
+//
+//  Created by Samuel Eskenasy on 10/07/2017.
+//  Copyright © 2017 Samuel Eskenasy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
